8c43b5b8ee5d8c9d50fd5a37d5e0d6ea91e70447
Sascha Graeff <sascha@blackicegames.de>
2015-02-16 18:29:39 +0100

    Waffen-Auswahl-GUI hinzugefuegt.

---
+++ Projekt/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    private static PlayerWeapon _currentWeapon;
+    public static PlayerWeapon currentWeapon
+    {
+        get { return _currentWeapon; }
+        set
+        {
+            if(_currentWeapon)
+                _currentWeapon.Deactivate();
+            _currentWeapon = value;
+            _currentWeapon.Activate();
+        }
+    }
+
+    private Image image;
+    public KeyCode keyToSelect;
+    public bool startWeapon;
+    private const float activeAlpha = 1;
+    private const float inactiveAlpha = 0.1f;
+    private const float fadeSpeed = 3;
+    private float targetAlpha = inactiveAlpha;
+
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Start()
+    {
+        if(startWeapon)
+        {
+            currentWeapon = this;
+        }
+    }
+
+	void Update()
+    {
+	    if(Input.GetKeyDown(keyToSelect))
+        {
+            currentWeapon = this;
+        }
+
+        var c = image.color;
+        c.a = Mathf.MoveTowards(c.a, targetAlpha, Time.deltaTime * fadeSpeed);
+        image.color = c;
+	}
+
+    private void Activate()
+    {
+        targetAlpha = activeAlpha;
+        PlayerWeaponSelector.instance.targetPosition = GetComponent<RectTransform>().position;
+    }
+    private void Deactivate()
+    {
+        targetAlpha = inactiveAlpha;
+    }
+}
---
+++ Projekt/Assets/Scripts/PlayerWeaponSelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeaponSelector : MonoBehaviour
+{
+    public static PlayerWeaponSelector instance { private set; get; }
+    private const float speed = 1000;
+    private RectTransform t;
+    [System.NonSerialized]
+    public Vector3 targetPosition;
+
+
+	void Awake()
+    {
+        instance = this;
+        t = GetComponent<RectTransform>();
+	}
+
+	void Update()
+    {
+        t.position = Vector3.MoveTowards(t.position, targetPosition, Time.deltaTime * speed);
+	}
+}
