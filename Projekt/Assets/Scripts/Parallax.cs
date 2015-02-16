7bd61a24448473126abab140c2319513e906f49f
Sascha Graeff <sascha@blackicegames.de>
2015-02-16 19:25:03 +0100

    Parallax-Script hinzugefuegt.

---
+++ Projekt/Assets/Scripts/Parallax.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[ExecuteInEditMode]
+public class Parallax : MonoBehaviour
+{
+    public Vector2 parallax;
+    public Vector2 offset;
+
+	void OnWillRenderObject()
+    {
+        var camPos = Camera.current.transform.position;
+        var z = transform.position.z;
+        Vector3 newPos = offset + Vector2.Scale(parallax, camPos);
+        newPos.z = z;
+        transform.position = newPos;
+	}
+}
