ab83bd5c5442e81f6d49bc636e9a95ca02a32a46
Sascha Graeff <sascha@blackicegames.de>
2015-02-16 19:24:27 +0100

    Die Kamera folgt der Kugel.

---
+++ Projekt/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+
+    void Start()
+    {
+        if(!target)
+        {
+            target = GameObject.FindWithTag("Player").transform;
+        }
+    }
+
+	void LateUpdate()
+    {
+        transform.position = target.position + offset;
+	}
+}
